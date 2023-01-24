6a3a343c4da1b13535809aa3895315114bce70f8
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-01-24 17:35:02 +0530

    secondcommit

--- src/test/java/Test/NewTest1.java
+++ src/test/java/Test/NewTest1.java
@@ -5,7 +5,7 @@ public class NewTest1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("good evening");
-		//successfully pushed
+		//successfully pulled from github
 	}
 
 }
